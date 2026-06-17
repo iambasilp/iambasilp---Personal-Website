b7e301b754c98f09c00bfa5cfb9c24a97546f6be
Basil The Intentional <basilpulikuth@Basils-MacBook-Air.local>
2026-06-17 15:03:03 +0530

    Add email mailto link to footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,6 +47,7 @@ export default function RootLayout({
 function Footer() {
   const links = [
     { name: '@iambasilp', url: 'https://x.com/iambasilp' },
+    { name: 'email', url: 'mailto:iambasilp@gmail.com' },
     { name: 'youtube', url: 'https://www.youtube.com/@iambasilp' },
     { name: 'linkedin', url: 'https://www.linkedin.com/in/iambasilp' },
     { name: 'github', url: 'https://github.com/iambasilp' }
